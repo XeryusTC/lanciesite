eec8498e929a6eb1096bfa03a8d49d2d53f24d4c
Xeryus Stokkel <armadillo@onenetbeyond.org>
2014-07-13 21:20:22 +0200

    Make the index use the pubsite app

--- lanciesite/urls.py
+++ lanciesite/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'lanciesite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', include('pubsite.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
