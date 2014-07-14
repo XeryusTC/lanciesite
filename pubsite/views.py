3ced4d690ac028c15c389da633fc1ae8177f4367
XeryusTC <armadillo@onenetbeyond.org>
2014-07-14 20:57:00 +0200

    Add the basic views and their associated templates

--- pubsite/views.py
+++ pubsite/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
 
-# Create your views here.
+class AboutView(TemplateView):
+    template_name = "pubsite/about.html"
+
+class ContactView(FormView):
+    template_name = "pubsite/contact.html"
+
+class IndexView(TemplateView):
+    template_name = "pubsite/index.html"
+
+class CheckListView(TemplateView):
+    template_name = "pubsite/checklist.html"
+
+class RegisterView(FormView):
+    template_name = "pubsite/register.html"
+
