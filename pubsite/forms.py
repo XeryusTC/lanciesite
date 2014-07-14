dbe3e4054d0d3a78a509e9e07606984164602ac3
XeryusTC <armadillo@onenetbeyond.org>
2014-07-14 21:46:57 +0200

    Add dummies for the forms

---
+++ pubsite/forms.py
@@ -0,0 +1,8 @@
+from django.forms import Form
+
+class ContactForm(Form):
+    pass
+
+
+class RegisterForm(Form):
+    pass
--- pubsite/views.py
+++ pubsite/views.py
@@ -2,18 +2,26 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
 
+from pubsite.forms import ContactForm, RegisterForm
+
 class AboutView(TemplateView):
     template_name = "pubsite/about.html"
 
+
 class ContactView(FormView):
     template_name = "pubsite/contact.html"
+    form_class = ContactForm
+
 
 class IndexView(TemplateView):
     template_name = "pubsite/index.html"
 
+
 class CheckListView(TemplateView):
     template_name = "pubsite/checklist.html"
 
+
 class RegisterView(FormView):
     template_name = "pubsite/register.html"
+    form_class = RegisterForm
 
