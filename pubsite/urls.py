173d7b8dab4aa30e757dcb2713431b42a9e63795
XeryusTC <armadillo@onenetbeyond.org>
2014-07-14 20:57:21 +0200

    Add urls for the pubsite and its current views

--- pubsite/urls.py
+++ pubsite/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, url
 from pubsite import views
 
-urlpatterns = patterns('',
+urlpatterns = patterns('views',
+    url(r'^$', views.IndexView.as_view(), name='home'),
+    url(r'^about/$', views.AboutView.as_view(), name='about'),
+    url(r'^checklist/$', views.CheckListView.as_view(), name='checklist'),
+    url(r'^contact/$', views.ContactView.as_view(), name='contact'),
+    url(r'^register/$', views.RegisterView.as_view(), name='register'),
 )
