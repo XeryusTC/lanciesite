8ddf81bbb2e3958f718e1df66a8c29aba985730d
XeryusTC <armadillo@onenetbeyond.org>
2014-08-22 13:50:49 +0200

    Update templates to include participant overview.
    - Add a menu
    - Add additional templates
    - Show form in buy menu for debugging

--- pointofsale/urls.py
+++ pointofsale/urls.py
@@ -3,4 +3,5 @@ from pointofsale import views
 
 urlpatterns = patterns('',
     url(r'^$', views.BuyDrinkView.as_view(), name="buydrink"),
+    url(r'^participants/$', views.ParticipantOverview.as_view(), name="participants"),
 )
--- pointofsale/views.py
+++ pointofsale/views.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
 
 from pointofsale.models import Drink, Account, DrinkOrder
@@ -10,3 +11,6 @@ class BuyDrinkView(FormView):
     template_name = "pointofsale/buydrink.html"
     form_class = BuyDrinkForm
     success_url = reverse_lazy("pos:buy_drink")
+
+class ParticipantOverview(TemplateView):
+    template_name = "pointofsale/participants.html"
