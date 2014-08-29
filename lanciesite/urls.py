f8a32b021762aa453e297beb6a919b76db6f9d96
Marijn Pool <info@marijnpool.nl>
2014-08-29 23:05:26 +0200

    Updated Global template settings
    changes to new bootstrap based template, idea for future is to implement the variables in the template again(if necessary)

--- lanciesite/urls.py
+++ lanciesite/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from lanciesite.views import BaseStyleView
+#from lanciesite.views import BaseStyleView
 
 urlpatterns = patterns('',
     # Examples:
@@ -11,6 +11,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^base_style.css$', BaseStyleView.as_view(), name="base_style"),
+    #url(r'^base_style.css$', BaseStyleView.as_view(), name="base_style"),
     url(r'^', include('pubsite.urls', namespace="pub")),
 )
